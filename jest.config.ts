3f69c7da874268b39115e4a0bca3eb92f27a9c1a
ilyats <iluha3211@gmail.com>
2025-12-25 12:32:35 +0200

    add user service tests

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,8 @@ import { Config } from '@jest/types';
 const config: Config.InitialOptions = {
     verbose: true,
     preset: 'ts-jest',
+    testEnvironment: 'node',
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
 
 export default config;
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,8 +1,94 @@
 import { Container } from 'inversify';
-const container = new Container();
+import { IConfigService } from '../config/config.service.interface';
+import { IUsersRepository } from './users.repository.interface';
+import { IUserService } from './user.service.interface';
+import { TYPES } from '../types';
+import { UserService } from './user.service';
+import { User } from './user.entity';
 
-beforeAll(() => {});
+const ConfigServiceMock: IConfigService = {
+    get: jest.fn(),
+};
 
-describe('User Service', () => {
-    it('createUser', async () => {});
+const UsersRepositoryMock: IUsersRepository = {
+    find: jest.fn(),
+    create: jest.fn(),
+};
+
+let container: Container;
+let configService: IConfigService;
+let usersRepository: IUsersRepository;
+let usersService: IUserService;
+
+beforeAll(() => {
+    container = new Container();
+    container.bind<IUserService>(TYPES.UserService).to(UserService);
+    container.bind<IConfigService>(TYPES.ConfigService).toConstantValue(ConfigServiceMock);
+    container.bind<IUsersRepository>(TYPES.UsersRepository).toConstantValue(UsersRepositoryMock);
+
+    configService = container.get<IConfigService>(TYPES.ConfigService);
+    usersRepository = container.get<IUsersRepository>(TYPES.UsersRepository);
+    usersService = container.get<IUserService>(TYPES.UserService);
+});
+
+describe('createUser', () => {
+    it('success', async () => {
+        configService.get = jest.fn().mockReturnValueOnce('1');
+        usersRepository.create = jest.fn().mockImplementationOnce((user: User) => ({
+            id: 1,
+            email: user.email,
+            name: user.name,
+            password: user.password,
+        }));
+
+        const createdUser = await usersService.createUser({
+            email: 'a@a.com',
+            name: 'User',
+            password: 'qwerty123',
+        });
+
+        expect(createdUser?.id).toEqual(1);
+        expect(createdUser?.password).not.toEqual('qwerty123');
+    });
+});
+
+describe('validateUser', () => {
+    beforeAll(() => {
+        usersRepository.find = jest.fn().mockResolvedValueOnce({
+            email: 'a@a.com',
+            name: 'User',
+            password: 'qwerty123',
+        });
+    });
+
+    it('correct password', async () => {
+        jest.spyOn(User.prototype, 'comparePassword').mockResolvedValue(true);
+
+        const isValid = await usersService.validateUser({
+            email: 'a@a.com',
+            password: 'qwerty123',
+        });
+
+        expect(isValid).toBeTruthy();
+    });
+
+    it('wrong password', async () => {
+        const isValid = await usersService.validateUser({
+            email: 'a@a.com',
+            password: '1',
+        });
+
+        expect(isValid).toBeFalsy();
+    });
+
+    it('wrong user', async () => {
+        usersRepository.find = jest.fn().mockResolvedValueOnce(null);
+
+        const isValid = await usersService.validateUser({
+            email: 'a@a.com',
+            password: 'qwerty123',
+        });
+
+        expect(isValid).toBeFalsy();
+    });
 });
